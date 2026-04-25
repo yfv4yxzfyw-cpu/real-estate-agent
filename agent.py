81c6ce8e6bed704a8411fb8748a70e3895420109
yfv4yxzfyw-cpu <yfv4yxzfyw@privaterelay.appleid.com>
2026-04-25 18:24:44 +0300

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,8 +1,12 @@
 import requests
 
 BOT_TOKEN = "8651671485:AAE1uudOzDkBDvW2OaVrKQZoIomb5hxTSy4"
+CHAT_ID = 1160495820
 
-# يجيب آخر رسالة (وفيها chat_id)
-res = requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/getUpdates").json()
-
-print(res)
+requests.post(
+    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+    data={
+        "chat_id": CHAT_ID,
+        "text": "🔥 مبروك! البوت شغال 100%"
+    }
+)
