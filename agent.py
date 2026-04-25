8f9f8ee3d0e3b0b41838035829cb740baee413eb
yfv4yxzfyw-cpu <yfv4yxzfyw@privaterelay.appleid.com>
2026-04-25 18:29:16 +0300

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,12 +1,34 @@
 import requests
+from bs4 import BeautifulSoup
 
 BOT_TOKEN = "8651671485:AAE1uudOzDkBDvW2OaVrKQZoIomb5hxTSy4"
 CHAT_ID = 1160495820
 
-requests.post(
-    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-    data={
-        "chat_id": CHAT_ID,
-        "text": "🔥 مبروك! البوت شغال 100%"
-    }
-)
+def send(msg):
+    requests.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        data={"chat_id": CHAT_ID, "text": msg}
+    )
+
+url = "https://haraj.com.sa/search/اراضي-للبيع-الرياض"
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+res = requests.get(url, headers=headers)
+soup = BeautifulSoup(res.text, "html.parser")
+
+ads = soup.find_all("a")
+
+count = 0
+
+for ad in ads:
+    text = ad.get_text()
+
+    if "الرياض" in text and ("ارض" in text or "فيلا" in text):
+        send(f"🔎 إعلان:\n{text}")
+        count += 1
+
+    if count >= 5:
+        break
