c82a38f09ff36939b33261e67cfac7e5e663cfa5
yfv4yxzfyw-cpu <yfv4yxzfyw@privaterelay.appleid.com>
2026-04-25 18:15:04 +0300

    Update agent.py

--- agent.py
+++ agent.py
@@ -2,15 +2,7 @@ import requests
 
 BOT_TOKEN = "8651671485:AAE1uudOzDkBDvW2OaVrKQZoIomb5hxTSy4"
 
-# يجيب آخر محادثة (chat_id) تلقائي
+# يجيب آخر رسالة (وفيها chat_id)
 res = requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/getUpdates").json()
 
-if not res["result"]:
-    print("❌ ما فيه محادثة - لازم تضغط Start للبوت")
-else:
-    chat_id = res["result"][-1]["message"]["chat"]["id"]
-
-    msg = "🔥 البوت شغال 100%"
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-
-    requests.post(url, data={"chat_id": chat_id, "text": msg})
+print(res)
